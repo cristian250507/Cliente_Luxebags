e2b73d8b99d91954c348291854ed2bac751938ed
Cristian <vina@duoc.acad>
2025-05-09 09:30:22 -0400

    nuevo proyecto

---
+++ Cliente_Luxebags/src/main/java/com/luxebags_cli/cl/LuxeBags/ClienteLuxebagsApplication.java
@@ -0,0 +1,13 @@
+package com.luxebags_cli.cl.LuxeBags;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteLuxebagsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteLuxebagsApplication.class, args);
+	}
+
+}
---
+++ Cliente_Luxebags/src/test/java/com/luxebags_cli/cl/LuxeBags/ClienteLuxebagsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luxebags_cli.cl.LuxeBags;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteLuxebagsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
