986b0490c4826cf0f6a1b1a555114fb681047a7c
Cristian <cris.pizarrob@duocuc.cl>
2025-05-20 23:55:48 -0400

    agrego validaciones para el rut del cliente

---
+++ src/main/java/Cliente_LuxeBags/ClienteLuxeBags/Repository/ClienteRepository.java
@@ -0,0 +1,16 @@
+package Cliente_LuxeBags.ClienteLuxeBags.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import Cliente_LuxeBags.ClienteLuxeBags.Model.Cliente;
+
+
+
+@Repository
+//agrupa metodos
+public interface ClienteRepository extends JpaRepository<Cliente ,Integer >{
+
+    Cliente findByRut(String rut);
+
+}
